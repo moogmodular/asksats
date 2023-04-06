e63aec3373f9d6c1ac594c1093dd021c7724ead8
Schlaus Kwab <schlauskwab@proton.me>
2023-04-06 16:33:55 +0200

    fix: about refactor

--- src/components/stats/SidebarAbout.tsx
+++ src/components/stats/SidebarAbout.tsx
@@ -1,7 +1,7 @@
 import { trpc } from '~/utils/trpc'
 import { MDRender } from '~/components/common/MDRender'
 import BookIcon from '@mui/icons-material/Book'
-import { IconButton } from '@mui/material'
+import { IconButton, Tooltip } from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import NextLink from 'next/link'
 import Link from '@mui/material/Link'
@@ -16,36 +16,39 @@ export const SidebarAbout = ({}: SidebarAboutProps) => {
             <h2>About:</h2>
             <img src={'/artisats_big_logo.svg'} className={'h-48 w-48'} />
             {data && <MDRender content={data.message} />}
-            <div className={'flex grow flex-col items-center justify-between'}>
+            <div className={'justify-betweenm flex grow flex-row items-center'}>
                 <div className={'flex flex-row items-center'}>
-                    <b>Blog</b>
-                    <IconButton color="primary" id={'open-authenticate-button'}>
-                        <NextLink href={`/ask/blog`}>
-                            <BookIcon fontSize={'large'} />
-                        </NextLink>
-                    </IconButton>
+                    <Tooltip title={`blog`}>
+                        <IconButton color="primary" id={'open-authenticate-button'}>
+                            <NextLink href={`/ask/blog`}>
+                                <BookIcon fontSize={'large'} />
+                            </NextLink>
+                        </IconButton>
+                    </Tooltip>
                 </div>
                 <div className={'flex flex-row items-center'}>
-                    <b>Source</b>
-                    <IconButton color="primary" id={'open-authenticate-button'}>
-                        <Link href={`https://github.com/zerealschlauskwab/asksats`}>
-                            <GitHubIcon fontSize={'large'} />
-                        </Link>
-                    </IconButton>
+                    <Tooltip title={`source`}>
+                        <IconButton color="primary" id={'open-authenticate-button'}>
+                            <Link href={`https://github.com/zerealschlauskwab/asksats`}>
+                                <GitHubIcon fontSize={'large'} />
+                            </Link>
+                        </IconButton>
+                    </Tooltip>
                 </div>
+
                 <div className={'flex flex-row items-center'}>
-                    <b>Nostr:</b>
-                    <i>b3ce84d464f119d89ab14a636c6cfcb4da0ac7660b1bf965a1c32bfc9e836eab</i>
-                </div>
-                <div className={'flex flex-row items-center'}>
-                    <b>Twitter: </b>
-                    <IconButton color="primary" id={'open-authenticate-button'}>
-                        <Link href={`https://twitter.com/artisatscom`}>
-                            <TwitterIcon fontSize={'large'} />
-                        </Link>
-                    </IconButton>
+                    <Tooltip title={`twitter`}>
+                        <IconButton color="primary" id={'open-authenticate-button'}>
+                            <Link href={`https://twitter.com/artisatscom`}>
+                                <TwitterIcon fontSize={'large'} />
+                            </Link>
+                        </IconButton>
+                    </Tooltip>
                 </div>
             </div>
+            <Tooltip title={`nostr`}>
+                <i>b3ce84d464f119d89ab14a636c6cfcb4da0ac7660b1bf965a1c32bfc9e836eab</i>
+            </Tooltip>
         </div>
     )
 }
